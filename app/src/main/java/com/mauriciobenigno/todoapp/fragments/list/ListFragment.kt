55af70c0118728e95f4322c13d8430e3e08de3da
Maurício Benigno <mauriciobenigno@hotmail.com>
2023-02-26 11:06:58 -0300

    implement elements in add and update fragments

--- app/src/main/java/com/mauriciobenigno/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/mauriciobenigno/todoapp/fragments/list/ListFragment.kt
@@ -23,6 +23,10 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
+        binding.rootLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
         return binding.root
     }
 
