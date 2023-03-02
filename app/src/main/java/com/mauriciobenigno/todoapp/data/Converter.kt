4d7e459f6d2a17f92c332f8fff7de1051cc286b7
Maurício Benigno <mauriciobenigno@hotmail.com>
2023-03-02 15:50:59 -0300

    implementation converter to Priority

---
+++ app/src/main/java/com/mauriciobenigno/todoapp/data/Converter.kt
@@ -0,0 +1,17 @@
+package com.mauriciobenigno.todoapp.data
+
+import androidx.room.TypeConverter
+
+class Converter {
+
+    @TypeConverter
+    fun fromPriority(priority: Priority): String {
+        return priority.name
+    }
+
+    @TypeConverter
+    fun toPriority(priority: String) : Priority {
+        return Priority.valueOf(priority)
+    }
+
+}
